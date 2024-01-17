f6c99123abb87542eb6df1cb2a5739946f153d7e
romheat <romheat@gmail.com>
2024-01-17 12:35:59 +0000

    Init

---
+++ zsh/zalias.zsh
@@ -0,0 +1,8 @@
+
+# The following lines were added by compinstall
+alias reload="source ~/.zshrc"
+alias lh='ls -d .* --color' # show hidden files/directories only
+alias lsd='ls -aFhlG --color'
+alias l='ls -al --color'
+alias ls='ls -Fh --color' # Colorize output, add file type indicator, and put sizes in human readable format
+alias ll='ls -GFhl --color' # Same as above, but in long listing format
---
+++ zsh/zfunctions.zsh
@@ -0,0 +1,22 @@
+# Insert sudo at begining
+# Alt+s or ESC, s: inserts "sudo " at the start of line:
+function sudo-command-line() {
+    [[ -z $BUFFER ]] && zle up-history
+    [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
+    zle end-of-line
+}
+zle -N sudo-command-line
+
+# Change cursor shape in vi mode
+zle-keymap-select () {
+    if [[ $KEYMAP == vicmd ]]; then
+        # the command mode for vi
+        echo -ne "\e[5 q"
+    else
+        # the insert mode for vi
+        echo -ne "\e[2 q"
+    fi
+}
+precmd_functions+=(zle-keymap-select)
+zle -N zle-keymap-select
+echo -ne "\e[2 q"
