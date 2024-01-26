9304330aa5ad1556db60e154e5b74c9ecefbd2a4
romheat <romheat@gmail.com>
2024-01-26 19:30:50 +0000

    added arm64

--- req.sh
+++ req.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 
 # check if a package is installed if not install it
-# Usage: check_package <package_name>
+# Usage: check_package <package_name_x86> <package_name_arm>
 function check_package() {
-    if [ -z "$(dpkg -l | grep $1)" ]; then
-        echo "Package $1 is not installed"
-        echo "Installing $1"
-        sudo apt-get install $1
-        exit 1
-    fi
+  if [ -z "$2" ]; then
+      $2=$1
+  fi
+  if [ arch = "arm" ]; then
+      $1=$2
+  fi
+  if [ -z "$(dpkg -l | grep $1)" ]; then
+      echo "Package $1 is not installed"
+      echo "Installing $1"
+      sudo apt-get install $1
+  else
+      echo "Skip $1"
+  fi
 }
+
+function set_arch() {
+  if (uname -m | grep -q x86_64); then
+      arch = "x86_64"
+  elif (uname -m | grep -q arm); then
+      arch = "arm"
+  fi
+}
+
+set_arch
 
 mkdir -p ~/.cache/zsh
 mkdir -p ~/.cache/vim
@@ -20,7 +37,7 @@ check_package zsh-autosuggestions       # zsh autosuggestions
 check_package zsh-syntax-highlighting   # zsh syntax-highlighting command line
 check_package fd-find                   # better find (use with fuzzy find)
 check_package fzf                       # fuzzy find
-check_package batcat                    # better cat (used with fzf)
+check_package batcat bat                # better cat (used with fzf)
 
 # Link config files
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
@@ -28,5 +45,5 @@ ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 ln -s ~/.dotfiles/.Xdefaults ~/.Xdefaults
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
