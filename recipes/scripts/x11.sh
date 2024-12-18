ece981d5dbaaa6738b49ef99e7d014ff688050ab
romheat <romheat@gmail.com>
2024-12-18 10:43:50 +0100

    change comments

--- recipes/scripts/x11.sh
+++ recipes/scripts/x11.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 #DOC: Install base X11 server without window manager
 
+#  xserver-xorg-video-intel \  # check video card
+#  x11-xserver-utils \         # xset, xrand
+#  x11-utils \                 # xkill, xprops, xwinfo
+#  xfonts-base \               # fonts
+#  rxvt-unicode \              # term
+
 # X11
 sudo apt install \
   xorg \
   xserver-xorg-core \
-  xserver-xorg-video-intel \  # check video card
-  x11-xserver-utils \         # xset, xrand
-  x11-utils \                 # xkill, xprops, xwinfo
-  xfonts-base \               # fonts
-  rxvt-unicode \              # term
+  xserver-xorg-video-intel \  
+  x11-xserver-utils \         
+  x11-utils \                 
+  xfonts-base \               
+  rxvt-unicode \              
   xterm           
 
