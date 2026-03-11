d59a943f0edf38ccc13888ae3dc346cbac1749e0
romheat <romheat@gmail.com>
2026-03-11 08:00:55 +0100

    chg: add xclip and clean

--- zsh/zexports.zsh
+++ zsh/zexports.zsh
@@ -1,10 +1,9 @@
 # Exports
 export CLICOLOR=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
-export TERM=xterm-256color
+#export TERM=xterm-256color
 export TERM=screen-256color
 export LESS="--ignore-case --raw-control-chars"
-export PAGER="less"
 export EDITOR="vim"
 export ZDOTDIR=~/.dotfiles/zsh
 export SHELL=/usr/bin/zsh
@@ -14,7 +13,6 @@ export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
 export MANROFFOPT="-c"
 
 # options
-export GREP_OPTIONS="--color=always"
 export GREP_COLORS='ms=01;34:mc=01;34:sl=:cx=:fn=35:ln=32:bn=32:se=36'
 
 # fzf
@@ -38,8 +36,8 @@ export FZF_ALT_C_COMMAND="fdfind \
 --exclude node_modules \
 . $1 \
 "
-
-export FZF_DEFAULT_OPTS="--ansi --layout=reverse --info=inline --height=80% --preview 'batcat --style=numbers --color=always --line-range :500 {}' --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008' --prompt='∼ ' --pointer='▶' --marker='✓' --bind '?:toggle-preview' --bind 'ctrl-a:select-all' --bind 'ctrl-y:execute-silent(echo {+} | pbcopy)' --bind 'ctrl-e:execute(echo {+} | xargs -o vim)' --bind 'ctrl-v:execute(code {+})'"
+# pbcopy / xclip -selection clipboard
+export FZF_DEFAULT_OPTS="--ansi --layout=reverse --info=inline --height=80% --preview 'batcat --style=numbers --color=always --line-range :500 {}' --color='hl:148,hl+:154,pointer:032,marker:010,bg+:237,gutter:008' --prompt='∼ ' --pointer='▶' --marker='✓' --bind '?:toggle-preview' --bind 'ctrl-a:select-all' --bind 'ctrl-y:execute-silent(echo {+} | xclip -selection clipboard)' --bind 'ctrl-e:execute(echo {+} | xargs -o vim)' --bind 'ctrl-v:execute(code {+})'"
 
 export FZF_CTRL_T_OPTS="--ansi --preview 'batcat -n --color=always {}' --bind '?:toggle-preview' "
 export FZF_CTRL_R_OPTS="--no-preview"
