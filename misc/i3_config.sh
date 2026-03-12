9ec24606245385e086a0f0918a639bcec1d43721
romheat <romheat@gmail.com>
2026-03-12 09:07:49 +0100

    fix: add fonts

--- misc/i3_config.sh
+++ misc/i3_config.sh
@@ -1,2 +1,4 @@
-cp -r ~/.dotfiles/misc/.config ~/.config
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+cp -r ~/.dotfiles/misc/.config/* ~/.config
 cp ~/.dotfiles/misc/.Xdefaults ~/.Xdefaults
